d634064b342b70154e91a2986a4eee68f7104e80
Shabari03 <shabari@tenz.ai>
2025-11-30 19:38:36 +0530

    Add Terraform provider and S3 bucket configuration

---
+++ day4/task-solution/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-backend-aws-1"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
